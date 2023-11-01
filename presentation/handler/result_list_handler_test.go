82ed2d67d3055ab55f1cd7c9ec88e6a2d880997e
enamishunsuke <enami-s@asahi.com>
2023-11-01 17:34:08 +0900

    もぐさん閲覧用

--- presentation/handler/result_list_handler_test.go
+++ presentation/handler/result_list_handler_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 // SQLiteにじゃんけんした結果を1レコード挿入する関数
-func insertResult(hand string, result string) error {
+func insertResult(user_hand string, computer_hand string, result string) error {
 	//SQLiteを用いてDBを作成する
 	db, err := sql.Open("sqlite3", "./janken.db")
 	//DBを閉じる
@@ -19,7 +19,7 @@ func insertResult(hand string, result string) error {
 		return err
 	}
 	//DBにじゃんけんの結果を保存する
-	_, err = db.Exec("INSERT INTO results (hand, result) VALUES (?, ?)", hand, result)
+	_, err = db.Exec("INSERT INTO janken_results(user_hand, computer_hand, result) VALUES(?, ?, ?)", user_hand, computer_hand, result)
 
 	if err != nil {
 		return err
@@ -44,27 +44,41 @@ func deleteAllResults() error {
 	return nil
 }
 
+type GameResult struct {
+	ComputerHand string `json:"computer_hand"`
+	UserHand     string `json:"user_hand"`
+	Result       string `json:"result"`
+}
+
 func TestResultListHandler(t *testing.T) {
 	// テストケース1: じゃんけん結果一覧の取得
 	t.Run("Get Results with limit and offset", func(t *testing.T) {
 		// テストデータを3つ挿入
-		insertResult("rock", "win")
-		insertResult("scissors", "lose")
-		insertResult("paper", "tie")
+		insertResult("rock", "scissors", "win")
+		insertResult("scissors", "rock", "lose")
+		insertResult("paper", "paper", "draw")
 
 		request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/results?limit=2&offset=0", nil)
 		recorder := httptest.NewRecorder()
 
+		//ResultListHandlerの実行
 		ResultListHandler(recorder, request)
+		//recorder.Body.String()を使用して、recorderから返された全てのレスポンスボディをログに出力します。
+		t.Logf("Response Body: %s", recorder.Body.String())
 
 		var response []map[string]string
 		json.Unmarshal(recorder.Body.Bytes(), &response)
+
+		//json.Unmarshal(recorder.Body.Bytes(), &response) のエラーをチェック
+		if err := json.Unmarshal(recorder.Body.Bytes(), &response); err != nil {
+			t.Errorf("Error: %s", err)
+		}
 
 		// 期待される結果: 3つのうち、2つの結果が取得できる(中身もあっているのか確認)
 		if len(response) != 2 {
 			t.Errorf("Expected 2 results, but got: %d", len(response))
 		}
-		if response[0]["hand"] != "rock" || response[0]["result"] != "win" {
+		if response[0]["user_hand"] != "rock" || response[0]["result"] != "win" {
 			t.Errorf("Expected hand: rock, result: win, but got: hand: %s, result: %s", response[0]["hand"], response[0]["result"])
 		}
 		if response[1]["hand"] != "scissors" || response[1]["result"] != "lose" {
@@ -78,11 +92,11 @@ func TestResultListHandler(t *testing.T) {
 	// テストケース2: 別のoffsetでのじゃんけん結果一覧の取得
 	t.Run("Get Results with different limit and offset", func(t *testing.T) {
 		// テストデータを5つ挿入
-		insertResult("rock", "win")
-		insertResult("scissors", "lose")
-		insertResult("paper", "tie")
-		insertResult("rock", "win")
-		insertResult("scissors", "lose")
+		insertResult("rock", "scissors", "win")
+		insertResult("scissors", "rock", "lose")
+		insertResult("paper", "paper", "draw")
+		insertResult("rock", "scissors", "win")
+		insertResult("scissors", "rock", "lose")
 
 		request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/results?limit=2&offset=1", nil)
 		recorder := httptest.NewRecorder()
